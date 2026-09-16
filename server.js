f6dbeffe8913aa3aeb589684913f94e45713363f
vishal-gupta-connect-in <vishal.gupta.connect.in@gmail.com>
2026-09-17 01:29:35 +0530

    Created new port variable to share port dynamically

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express= require("express")
 const app = express()
-
+const port=3000
 
 app.get("/",(req,res)=>{
 res.send("Hi Everyone.")
 })
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("Backend Server is running on port number 5000")
 })
